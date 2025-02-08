c50280a48247f315d6874e6372cd574603515d3a
ranndy <ranndy180@gmail.com>
2025-02-08 13:16:15 -0500

    Actualizacion mas scripts

---
+++ herramientas/script-Procedures SQL Ejecutando código en MySQL/2) script-del-stored-incluye-producto-parametros.sql
@@ -0,0 +1,6 @@
+CALL incluye_producto_parametros('1000800','Sabor del Mar',
+'Naranja', '700 ml', 'Botella de Vidrio', 2.25);
+
+SELECT * FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = '1000800';
+
+
