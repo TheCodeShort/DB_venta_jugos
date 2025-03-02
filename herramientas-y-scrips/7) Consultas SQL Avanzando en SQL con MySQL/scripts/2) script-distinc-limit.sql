843992669955c37c9f832eeeeaf44310da0103ea
ranndy <ranndy180@gmail.com>
2025-03-02 10:21:36 -0500

    aTUALIZACION clase 7 broup by

--- herramientas-y-scrips/7) Consultas SQL Avanzando en SQL con MySQL/scripts/2) script-distinc-limit.sql
+++ herramientas-y-scrips/7) Consultas SQL Avanzando en SQL con MySQL/scripts/2) script-distinc-limit.sql
@@ -16,3 +16,18 @@ SELECT * FROM tabla_de_productos LIMIT 5;#limita el daato a mostrar
 SELECT * FROM tabla_de_productos LIMIT 5,4;#el primer numero es el idice osea desde donde va a empezar y el segundo es la cantidad que se va a mostrar
 
 SELECT * FROM tabla_de_vendedores WHERE FECHA_ADMISION = 2017-01-01 LIMIT 10;
+
+#ORDER BY tambien ordena de forma alfabetica o dle 1 al n 
+SELECT * FROM tabla_de_productos ORDER BY PRECIO_DE_LISTA;#aca se ordena de forma ASENDENTE por predeterminado
+
+SELECT * FROM tabla_de_productos ORDER BY PRECIO_DE_LISTA DESC;#se ordena de forma decendente pero si toca especificarlo 
+
+SELECT ESTADO, LIMITE_DE_CREDITO FROM tabla_de_clientes;
+
+
+#optenemos el producto con las especificaciones del SQL
+SELECT CODIGO_DEL_PRODUCTO FROM tabla_de_productos 
+WHERE NOMBRE_DEL_PRODUCTO = "Refrescante" AND TAMANO = "1 Litro" 
+AND SABOR = "Frutilla/Limón"; 
+
+SELECT * FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035" ORDER BY CANTIDAD DESC;
