3d69442d04d41990fc30f5e5beb9186b1460e6a2
ranndyy <ranndy180@gmail.com>
2025-03-02 11:58:01 -0500

    Actualizacin clase 7

--- herramientas-y-scrips/7) Consultas SQL Avanzando en SQL con MySQL/scripts/2) script-distinc-limit.sql
+++ herramientas-y-scrips/7) Consultas SQL Avanzando en SQL con MySQL/scripts/2) script-distinc-limit.sql
@@ -31,3 +31,73 @@ WHERE NOMBRE_DEL_PRODUCTO = "Refrescante" AND TAMANO = "1 Litro"
 AND SABOR = "Frutilla/Limón"; 
 
 SELECT * FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035" ORDER BY CANTIDAD DESC;
+
+#es bueno darle un alias a una operacion, tener encuenta que para poner un alias se utiliza AS	
+SELECT ESTADO, SUM(LIMITE_DE_CREDITO) AS LIMITE_TOTAL
+FROM tabla_de_clientes GROUP BY ESTADO;
+
+#tener encuenta que el filtro se poner antes que el GROUP BY 
+SELECT CIUDAD, BARRIO, SUM(LIMITE_DE_CREDITO) AS LIMITE 
+FROM tabla_de_clientes WHERE CIUDAD = 'CIUDAD DE MÉXICO'
+GROUP BY BARRIO;
+
+#¿Cuántos ítems vendidos cuentan con la mayor cantidad del producto '1101035'?
+SELECT MAX(CANTIDAD) AS CANTIDAD_MAXIMA FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035";
+SELECT COUNT(*) FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035" AND CANTIDAD = 99;
+
+
+#tener encuenta que el HEAVING va despues del GROUP BY 
+SELECT ESTADO, SUM(LIMITE_DE_CREDITO) AS LIMITE_TOTAL
+FROM tabla_de_clientes 
+GROUP BY ESTADO
+HAVING LIMITE_TOTAL > 300000;
+
+
+SELECT ENVASE, MAX(PRECIO_DE_LISTA) AS PRECIO_MAXIMO,
+MIN(PRECIO_DE_LISTA) AS PRECIO_MINIMO,
+SUM(PRECIO_DE_LISTA) AS SUMA_PRECIO
+FROM tabla_de_productos GROUP BY ENVASE
+HAVING SUM(PRECIO_DE_LISTA) >= 80 
+AND MAX(PRECIO_DE_LISTA) >= 5;
+
+select DNI, count(*) as conteo from facturas
+where YEAR (FECHA_VENTA) = 2016
+group by DNI
+having count(*) > 2000 ;
+
+
+#hace un test a uno o mas campos y dependiendo el resultado obtendremos un valor especifico
+SELECT NOMBRE_DEL_PRODUCTO, PRECIO_DE_LISTA,
+CASE
+   WHEN PRECIO_DE_LISTA >= 12 THEN 'Costoso'
+   WHEN PRECIO_DE_LISTA >= 5 AND PRECIO_DE_LISTA < 12 THEN 'Asequible'
+   ELSE 'Barato'
+END AS PRECIO #ner encuenta que se le asigna el alias
+FROM tabla_de_productos;
+
+
+SELECT ENVASE, SABOR,
+CASE
+   WHEN PRECIO_DE_LISTA >= 12 THEN 'Costoso'
+   WHEN PRECIO_DE_LISTA >= 5 AND PRECIO_DE_LISTA < 12 THEN 'Asequible'
+   ELSE 'Barato'
+END AS PRECIO, MIN(PRECIO_DE_LISTA) AS PRECIO_MINIMO
+FROM tabla_de_productos
+WHERE TAMANO = '700 ml'
+GROUP BY ENVASE,
+CASE
+   WHEN PRECIO_DE_LISTA >= 12 THEN 'Costoso'
+   WHEN PRECIO_DE_LISTA >= 5 AND PRECIO_DE_LISTA < 12 THEN 'Asequible'
+   ELSE 'Barato'
+END
+ORDER BY ENVASE;
+
+
+SELECT NOMBRE,
+CASE 
+    WHEN YEAR(fecha_de_nacimiento) < 1990 THEN 'Viejos'
+    WHEN YEAR(fecha_de_nacimiento) >= 1990 
+    AND YEAR(fecha_de_nacimiento) <= 1995 THEN 'Jóvenes' 
+    ELSE 'Niños' 
+END AS CLASIFICACION_EDAD
+FROM tabla_de_clientes;
